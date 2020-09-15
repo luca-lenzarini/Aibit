9ad2746946af50a0aed447e7ab4a8055133e90d9
Luca Lenzarini <luca.lenzarini@hotmail.com>
2020-09-15 00:16:24 -0300

    adicionado sprites de player teste e criado script de movimentacao

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public Rigidbody2D rigidbody;
+    public Animator animator;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update() {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    void FixedUpdate() {
+        // 
+        rigidbody.MovePosition(rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
