7f459aa51fb3c94889ae90e74a44438bb2d06220
Luca Lenzarini <luca.lenzarini@hotmail.com>
2020-11-17 00:40:03 -0300

    ranged enemy

---
+++ Assets/ProjectileController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public float damage = 15;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")) { 
+            other.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+            Destroy(gameObject);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/EnemyRangedAttack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRangedAttack : MonoBehaviour
+{
+    public float attackDelay;
+    private Transform target;
+
+    private float lastAttackTime;
+    public GameObject projectile;
+    public float projectileForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        Vector3 targetDir = target.position - transform.position;
+        float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 90 * Time.deltaTime);
+
+        if(Time.time > lastAttackTime + attackDelay) {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, 5);
+
+            Debug.DrawRay(transform.position, transform.up, Color.green);
+
+            if(hit.transform == target) {
+            
+                GameObject projectileItem = Instantiate(projectile, transform.position, transform.rotation);
+                projectileItem.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0f, projectileForce));
+                lastAttackTime = Time.time;
+
+                StartCoroutine(DestroyProjectile(projectileItem));
+            }
+        }
+    }
+
+    private IEnumerator DestroyProjectile(GameObject projectileItem) {
+        if(projectileItem != null) {
+            yield return new WaitForSeconds(3f);
+            Destroy(projectileItem);
+        }
+    }
+}
